2ec3c85722b880f0ce4e8b5be08cad190ba7ce82
Jared Holgate <jaredholgate@microsoft.com>
2025-05-13 22:31:18 +0100

    save changes

--- graph/graph.go
+++ graph/graph.go
@@ -30,46 +30,95 @@ func (graph *Graph) GetResources() ([]Resource, error) {
 
 	for _, subscriptionID := range graph.SubscriptionIDs {
 		fmt.Printf("Checking Subscription ID: %s\n", subscriptionID)
-		client, err := armresources.NewClient(subscriptionID, cred, nil)
-		if err != nil {
-			panic(err)
-		}
-		pager := client.NewListPager(nil)
 
-		for pager.More() {
-			page, err := pager.NextPage(context.Background())
-			if err != nil {
-				panic(err)
-			}
-
-			for _, resource := range page.Value {
-				// Check if the resource ID matches any of the ignore patterns
-				shouldIgnore := false
-				for _, pattern := range graph.IgnoreResourceIDPatterns {
-					matched, err := regexp.MatchString(pattern, *resource.ID)
-					if err != nil {
-						fmt.Printf("Error matching pattern %s: %v\n", pattern, err)
-						continue
-					}
-					if matched {
-						shouldIgnore = true
-						break
-					}
-				}
-				if shouldIgnore {
-					fmt.Printf("Ignoring Resource ID: %s\n", *resource.ID)
-					continue
-				}
-				fmt.Printf("Adding Resource ID: %s\n", *resource.ID)
-				resourceResult := Resource{
-					ID:   *resource.ID,
-					Type: *resource.Type,
-					Name: *resource.Name,
-				}
-				resources = append(resources, resourceResult)
-			}
-		}
+		resources = graph.getResourceGroups(subscriptionID, cred, resources)
+		resources = graph.getResources(subscriptionID, cred, resources)
 	}
 
 	return resources, nil
+}
+
+func (graph *Graph) getResourceGroups(subscriptionID string, cred *azidentity.DefaultAzureCredential, resources []Resource) []Resource {
+	resourceGroupsClient, err := armresources.NewResourceGroupsClient(subscriptionID, cred, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	pager := resourceGroupsClient.NewListPager(nil)
+	for pager.More() {
+		page, err := pager.NextPage(context.Background())
+		if err != nil {
+			panic(err)
+		}
+		for _, resourceGroup := range page.Value {
+			shouldIgnore := false
+			for _, pattern := range graph.IgnoreResourceIDPatterns {
+				matched, err := regexp.MatchString(pattern, *resourceGroup.ID)
+				if err != nil {
+					fmt.Printf("Error matching pattern %s: %v\n", pattern, err)
+					continue
+				}
+				if matched {
+					shouldIgnore = true
+					break
+				}
+			}
+			if shouldIgnore {
+				fmt.Printf("Ignoring Resource ID: %s\n", *resourceGroup.ID)
+				continue
+			}
+			fmt.Printf("Adding Resource ID: %s\n", *resourceGroup.ID)
+			resourceResult := Resource{
+				ID:   *resourceGroup.ID,
+				Type: *resourceGroup.Type,
+				Name: *resourceGroup.Name,
+			}
+			resources = append(resources, resourceResult)
+		}
+	}
+	return resources
+}
+
+func (graph *Graph) getResources(subscriptionID string, cred *azidentity.DefaultAzureCredential, resources []Resource) []Resource {
+	resourcesClient, err := armresources.NewClient(subscriptionID, cred, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	pager := resourcesClient.NewListPager(nil)
+
+	for pager.More() {
+		page, err := pager.NextPage(context.Background())
+		if err != nil {
+			panic(err)
+		}
+
+		for _, resource := range page.Value {
+			// Check if the resource ID matches any of the ignore patterns
+			shouldIgnore := false
+			for _, pattern := range graph.IgnoreResourceIDPatterns {
+				matched, err := regexp.MatchString(pattern, *resource.ID)
+				if err != nil {
+					fmt.Printf("Error matching pattern %s: %v\n", pattern, err)
+					continue
+				}
+				if matched {
+					shouldIgnore = true
+					break
+				}
+			}
+			if shouldIgnore {
+				fmt.Printf("Ignoring Resource ID: %s\n", *resource.ID)
+				continue
+			}
+			fmt.Printf("Adding Resource ID: %s\n", *resource.ID)
+			resourceResult := Resource{
+				ID:   *resource.ID,
+				Type: *resource.Type,
+				Name: *resource.Name,
+			}
+			resources = append(resources, resourceResult)
+		}
+	}
+	return resources
 }
